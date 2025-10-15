7f569ce105c206efa7425cbe1f5cb3a3b4badef5
Xiaokuang Bai <baixiaokuang@gmail.com>
2025-10-15 18:00:45 +0800

    implement text edit, prompt for template kind, trim markdown code block syntax.

--- llm-call-demo/src/extension.ts
+++ llm-call-demo/src/extension.ts
@@ -13,7 +13,45 @@ export function activate(context: vscode.ExtensionContext) {
       const selectedModelName = await vscode.window.showQuickPick(
         modelNameList
       );
-      const model = allModels.find((model) => model.name === selectedModelName);
+      const model =
+        allModels.find((model) => model.name === selectedModelName) ??
+        allModels[0];
+
+      const templateKind =
+        (await vscode.window.showInputBox({
+          prompt: "Which kind of template do you want to insert?",
+          value: "Rust",
+        })) ?? "Rust";
+
+      //   const text = textEditor.document.getText();
+
+      const messages = [
+        vscode.LanguageModelChatMessage.User(
+          `Generate ${templateKind} template for initialize a file`
+        ),
+      ];
+
+      try {
+        const response = await model.sendRequest(messages);
+
+        for await (const fragment of response.text) {
+          const content = fragment.replace(/^```.*$\n?/gm, "");
+          //   console.log(content);
+          await textEditor.edit((edit) => {
+            const lastLine = textEditor.document.lineAt(
+              textEditor.document.lineCount - 1
+            );
+            const position = new vscode.Position(
+              lastLine.lineNumber,
+              lastLine.text.length
+            );
+            edit.insert(position, content);
+          });
+        }
+      } catch (error) {
+        console.log(error);
+        return;
+      }
     }
   );
 
